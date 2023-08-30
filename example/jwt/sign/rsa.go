21c827a332978f2874fe2031c7675323f95b7e1c
ning <iscodd@gmail.com>
2023-08-30 19:34:53 +0800

    jwt

---
+++ example/jwt/sign/rsa.go
@@ -0,0 +1,49 @@
+package sign
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+type RsaSign struct {
+	publicKey  string
+	privateKey string
+}
+
+type rsaData struct {
+	Data []byte
+	jwt.RegisteredClaims
+}
+
+func NewRsaSign(privateKey, publicKey string) RsaSign {
+	return RsaSign{publicKey: publicKey, privateKey: privateKey}
+}
+
+func (s *RsaSign) Encode(data []byte) (string, error) {
+	d := rsaData{Data: data, RegisteredClaims: jwt.RegisteredClaims{
+		//Issuer:    "ADSF",
+		//Subject:   "63edecb5b654030011f12ea6",
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}}
+
+	t := jwt.NewWithClaims(jwt.SigningMethodRS512, d)
+	key, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(s.privateKey))
+	if err != nil {
+		return "", err
+	}
+
+	return t.SignedString(key)
+}
+
+func (s *RsaSign) Decode(sign string) ([]byte, error) {
+	out := rsaData{}
+	_, err := jwt.ParseWithClaims(sign, &out, func(token *jwt.Token) (interface{}, error) {
+		return jwt.ParseRSAPublicKeyFromPEM([]byte(s.publicKey))
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return out.Data, err
+}
